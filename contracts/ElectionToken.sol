de09dbe4d736717750c6ab13ac14e9fea90e65d6
Michael <mishaba1990@gmail.com>
2020-01-03 22:28:49 +0200

    updated admin

--- contracts/ElectionToken.sol
+++ contracts/ElectionToken.sol
@@ -18,11 +18,18 @@ contract ElectionToken is erc20, erc20Detailed, Owned{
     uint256 private expiration;    // contract expiration time *** 1577971200 =2/1/2020 13:20:00
     uint256 private start;         // contract start time
 
-    constructor()erc20Detailed("VotingToken", "VTC", 0) public{    
-     
+    /*constructor(uint256 _initialSupply, address[] memory _voterAddresses, address[] memory _candidateAddresses, uint256 _start, uint256 _end) erc20Detailed("VotingToken", "VTC", 0)public{    //_initialTokenSupply = number of voters
+        _mint(msg.sender, _initialSupply);  
+        addVotersList(_voterAddresses);
+        addCandidatesList(_candidateAddresses);
+        distributeTokens();
+        setElectionStartTime(_start);
+        setElectionExpirationTime(_end);
+    }*/
+    constructor() erc20Detailed("VotingToken", "VTC", 0)public onlyOwner{    //_initialTokenSupply = number of voters
     }
-    //_initialTokenSupply = number of voters
-    function cConstructor(uint256 _initialSupply, address[] memory _voterAddresses, address[] memory _candidateAddresses, uint256 _start, uint256 _end)  public{   
+
+    function initContract(uint256 _initialSupply, address[] memory _voterAddresses, address[] memory _candidateAddresses, uint256 _start, uint256 _end) public onlyOwner{
         _mint(msg.sender, _initialSupply);  
         addVotersList(_voterAddresses);
         addCandidatesList(_candidateAddresses);
--- src/js/admin.js
+++ src/js/admin.js
@@ -28,11 +28,11 @@ var Admin = {
     },
   
     initContract: function() {
-      $.getJSON("VTC.json", function(vtc) {
+      $.getJSON("ElectionToken.json", function(electiontoken) {
         // Instantiate a new truffle contract from the artifact
-        Admin.contracts.VTC = TruffleContract(vtc);
+        Admin.contracts.ElectionToken = TruffleContract(electiontoken);
         // Connect provider to interact with contract
-        Admin.contracts.VTC.setProvider(Admin.web3Provider);
+        Admin.contracts.ElectionToken.setProvider(Admin.web3Provider);
   
         //Admin.listenForEvents();
   
@@ -42,7 +42,7 @@ var Admin = {
   
     // Listen for events emitted from the contract
     // listenForEvents: function() {
-    //   Admin.contracts.VTC.deployed().then(function(instance) {
+    //   Admin.contracts.ElectionToken.deployed().then(function(instance) {
     //     // Restart Chrome if you are unable to receive this event
     //     // This is a known issue with Metamask
     //     // https://github.com/MetaMask/metamask-extension/issues/2393
@@ -65,54 +65,49 @@ var Admin = {
       let id = $(this).attr("id");
     switch (id) {
     case "1":
-          alert("This is first: " + id);
+          let input1=$(this).prev("input").val();
+                  
+          alert("This is first: " + input1);
           break;
       case "2":
-          alert("This is second: " + id);
+        let input2=$(this).prev("input").val();
+          alert("This is second: " + input2);
           break;
       case "3":
-          alert("This is third: " + id);
+        let input3=$(this).prev("input").val();
+          alert("This is third: " + input3);
         break;
         case "4":
-          alert("This is first: " + id);
+          let input4=$(this).prev("input").val();
+          alert("This is first: " + input4);
           break;
         case "5":
-          let input=$(this).prev("input").val();
-          let param1=input;
-          Admin.contracts.VTC.deployed().then(function(instance) {
-            instance.increaseSupply(param1,{}, {
-                    //    arguments: [arg],
-                      fromBlock: 0, 
-                      toBlock: 'latest'
-                    })
+          let input5=$(this).prev("input").val();
+          alert("This is first: " + input5);
+        //   Admin.contracts.ElectionToken.deployed().then(function(instance) {
+        //     instance.increaseSupply(param1,{}, {
+        //             //    arguments: [arg],
+        //               fromBlock: 0, 
+        //               toBlock: 'latest'
+        //             })
 
-        //    electionInstance = instance;
-        //   //  return electionInstance.increaseSupply();
-        //     alert("Ninja: "+ electionInstance.increaseSupply(param1,{}, {}).totalSupply);
-          });
-          break;
-        case "6":
-          alert("This is third: " + id);
+        // //    electionInstance = instance;
+        // //   //  return electionInstance.increaseSupply();
+        // //     alert("Ninja: "+ electionInstance.increaseSupply(param1,{}, {}).totalSupply);
+        //   });
           break;
           case "7":
             // let input=$(this).prev("input").val().split(",");
             // let input=$(this).prev("input").val();
             // let param1=input[0];
            
-            
-            // Admin.contracts.VTC.deployed().then(function(instance) {
+            // Admin.contracts.ELECTIONTOKEN.deployed().then(function(instance) {
             //   electionInstance = instance;
             //   debugger;
             //   // return electionInstance.candidatesCount();
             //   alert("Ninja: "|+" "+  electionInstance.transferAndAddVoterAddress(param1) );
             // });
           break;
-          case "8":
-            alert("This is second: " + id);
-            break;
-          case "9":
-            alert("This is third: " + id);
-            break;
       default:
           alert("No id :\(");
       break;
@@ -124,12 +119,9 @@ var Admin = {
       Admin.init();
     });
 
-
-
-
 $(document).ready(function () {
 
-    $("button").on("click", function() {
+    $("button").on("click","submit", function() {
     let val = $(this).prev("input").val();
         $("#output").text(val);
     });
