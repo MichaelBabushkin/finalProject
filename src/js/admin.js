0e5aa7880ea4016eda5bfe6c640655a04e6d9a87
Michael <mishaba1990@gmail.com>
2020-01-04 16:13:34 +0200

    get info from textfields

--- src/js/admin.js
+++ src/js/admin.js
@@ -60,69 +60,36 @@ var Admin = {
   
   };
   
-  // handle click 
-  $(".menu button").on("click", function(){
-      let id = $(this).attr("id");
-    switch (id) {
-    case "1":
-          let input1=$(this).prev("input").val();
-                  
-          alert("This is first: " + input1);
-          break;
-      case "2":
-        let input2=$(this).prev("input").val();
-          alert("This is second: " + input2);
-          break;
-      case "3":
-        let input3=$(this).prev("input").val();
-          alert("This is third: " + input3);
-        break;
-        case "4":
-          let input4=$(this).prev("input").val();
-          alert("This is first: " + input4);
-          break;
-        case "5":
-          let input5=$(this).prev("input").val();
-          alert("This is first: " + input5);
-        //   Admin.contracts.ElectionToken.deployed().then(function(instance) {
-        //     instance.increaseSupply(param1,{}, {
-        //             //    arguments: [arg],
-        //               fromBlock: 0, 
-        //               toBlock: 'latest'
-        //             })
 
-        // //    electionInstance = instance;
-        // //   //  return electionInstance.increaseSupply();
-        // //     alert("Ninja: "+ electionInstance.increaseSupply(param1,{}, {}).totalSupply);
-        //   });
-          break;
-          case "7":
-            // let input=$(this).prev("input").val().split(",");
-            // let input=$(this).prev("input").val();
-            // let param1=input[0];
-           
-            // Admin.contracts.ELECTIONTOKEN.deployed().then(function(instance) {
-            //   electionInstance = instance;
-            //   debugger;
-            //   // return electionInstance.candidatesCount();
-            //   alert("Ninja: "|+" "+  electionInstance.transferAndAddVoterAddress(param1) );
-            // });
-          break;
-      default:
-          alert("No id :\(");
-      break;
-    }
-  
-  })
-  
   $(document).ready(function() {
       Admin.init();
     });
 
 $(document).ready(function () {
 
-    $("button").on("click","submit", function() {
-    let val = $(this).prev("input").val();
-        $("#output").text(val);
-    });
-    });
+    $('#electionForm').on("submit",function(event) {
+      event.preventDefault();
+      // get all the inputs into an array.
+      var $inputs = $('#electionForm :input');
+  
+      // not sure if you wanted this, but I thought I'd add it.
+      // get an associative array of just the values.
+      var values = {};
+      $inputs.each(function(el) {
+       
+        if (el<5) {
+          values["input" + el] = $(this).val();
+        }
+         
+      });
+      Admin.contracts.ElectionToken.deployed().then(function(instance) {
+        instance.initContract(values.input1, values.input2, values.input3, 1578145360, 1578245360)
+          ,{}, {
+                 // arguments: [arg],
+                fromBlock: 0, 
+                toBlock: 'latest'
+              }
+      });
+  });
+
+})
